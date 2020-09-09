c787fcf4c3a53f280a5bbfaf7f8444e03ac6f21e
Clement LG <clementlegruiec@gmail.com>
2020-09-09 14:21:41 +0200

    base Saine Etp1

---
+++ src/transmetteurs/TransmetteurParfait.java
@@ -0,0 +1,38 @@
+package transmetteurs;
+
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConforme;
+
+
+/**
+ * Classe TransmetteurParfait herite d'un composant transmetteur d'informations
+ *
+ * @author c.legruiec
+ * @author e.leduc
+ * @author b.demoulin
+ */
+
+public class TransmetteurParfait extends Transmetteur<Boolean, Boolean> {
+
+    //canal Rx Information (abstract dans la classe mere)
+    public void recevoir(Information<Boolean> information) throws InformationNonConforme {
+        informationRecue = information;
+        emettre();//envoie l'information
+
+    }
+
+    //canl Tx Information (abstract dans la classe mere)
+    public void emettre() throws InformationNonConforme {
+        for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) {
+        	destinationConnectee.recevoir(informationRecue);
+        }
+        informationEmise = informationRecue;//transmetteur parfait src=dest
+
+    }
+
+
+}
+	
+
+
