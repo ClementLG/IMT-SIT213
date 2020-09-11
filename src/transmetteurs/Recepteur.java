f235b65cafb988b23ba4ff6d466a41edb80474aa
Clément <clementlegruiec@gmail.com>
2020-09-11 13:57:04 +0200

    maj emetteur OK

--- src/transmetteurs/Recepteur.java
+++ src/transmetteurs/Recepteur.java
@@ -1,9 +1,21 @@
 package transmetteurs;
 
+import information.Information;
 
 public class Recepteur{
+	private float Amax;
+	private float Amin;
+	private int nbEchantillon;
+	private String encodeType;
+	private Information<Float> informationConverti;
 	
-	
+	public Recepteur(float Amax, float Amin, int nbEchantillon, String decodeType) {
+		this.Amax=Amax;
+		this.Amin=Amin;
+		this.nbEchantillon=nbEchantillon;
+		this.encodeType=encodeType;
+		informationConverti =new Information<>();
+	}
 	
 	
 
