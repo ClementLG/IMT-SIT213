e2f9e6dbfc5b74b4ba2bf9c6608511396d1adf0e
ElouanLD <64096242+ElouanLD@users.noreply.github.com>
2020-09-23 15:43:50 +0200

    add : tests

---
+++ tests/transmetteurs/TransmetteurParfaitTest.java
@@ -0,0 +1,17 @@
+package transmetteurs;
+
+import information.InformationNonConforme;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransmetteurParfaitTest {
+
+    @Test
+    public void recevoir() throws InformationNonConforme {
+    }
+
+    @Test
+    public void emettre() throws InformationNonConforme {
+    }
+}
